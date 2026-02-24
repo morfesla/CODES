82040c29f762fd40f1427dbd4ddb3f84a8238917
morfesla <morfexsla@gmail.com>
2026-02-24 16:39:58 -0300

    Add files via upload

---
+++ SOMA.py
@@ -0,0 +1,14 @@
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+soma = num1 + num2
+produto = num1 * num2
+media = (num1 + num2) / 2
+maior = max(num1, num2)
+menor = min(num1, num2)
+
+print("Soma:", soma)
+print("Produto:", produto)
+print("Média:", media)
+print("Maior número:", maior)
+print("Menor número:", menor)
